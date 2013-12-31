7d45c8154be2a62909b6540c67bac4f6a9385930
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2013-12-31 12:23:19 +0200

    Copyright test. Constants.

--- src/Womgen/Womgen.php
+++ src/Womgen/Womgen.php
@@ -36,21 +36,24 @@
 
 			//    var_dump($portType);
 			echo "<?php\r\n";
-			echo "namespace {$wsNamespace};\r\n";
-			echo 'class '.$clientClassName." extends SoapClient\r\n";
-			echo "{\r\n";
-			echo "\tpublic function __construct(){   }\r\n";
+			echo "\tnamespace {$wsNamespace};\r\n";
+			echo \Womgen\Constants::LICENSE;
+			echo "\r\n";
+			echo "\tclass ".$clientClassName." extends SoapClient\r\n";
+			echo "\t{\r\n";
+			echo "\t\tpublic function __construct(){   }\r\n";
 			foreach($portType['children']['wsdl:operation'] as $operation)
 			{
-				echo "\r\n\t/*\r\n";
-				echo "\t*" . $operation['children']['wsdl:documentation'][0]['text'] . "\r\n";
+				echo "\r\n\t\t/*\r\n";
+				echo "\t\t*" . $operation['children']['wsdl:documentation'][0]['text'] . "\r\n";
 				echo "\t*/\r\n";
-				echo "\tpublic function " . $operation['attributes']['name'] ."(\\{$namespace}\\{$operation['children']['wsdl:input'][0]['attributes']['message']} \$in)\r\n";
-				echo "\t{\r\n";
-				echo "\t\treturn \$this->_call('{$operation['attributes']['name']}',\$in);\r\n";
-				echo "\r\n\t}\r\n";
+				$inClassName = str_replace('tns:','',$operation['children']['wsdl:input'][0]['attributes']['message']);
+				echo "\t\tpublic function " . $operation['attributes']['name'] ."(\\{$namespace}\\{$inClassName} \$in)\r\n";
+				echo "\t\t{\r\n";
+				echo "\t\t\treturn \$this->_call('{$operation['attributes']['name']}',\$in);\r\n";
+				echo "\r\n\t\t}\r\n";
 			}   
-			echo "}";
+			echo "\t}";
 			$code  = ob_get_clean();
 			file_put_contents($folder . '/' . $clientClassName . '.php',$code);
 			highlight_string($code);
